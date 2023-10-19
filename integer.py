19d6fed89937c3c5808bb1fec71a654e62df2fbb
Zak Goneau <zak.goneau@stu.ocsb.ca>
2023-10-19 16:05:53 +0000

    This program takes in a user input as an integer and will tell the user if their number is positive, negative or zero.

--- integer.py
+++ integer.py
@@ -8,15 +8,15 @@ def main():
     # Introduce program to user
     print(
         "This program gets the user input and tells them whether it's positive, negative or zero!\n"
-    )
+)
 
     # Get user number
     user_number = int(input("Please enter any number: "))
 
     # Check if user number is negative, positive or zero and display response
-    if user_number >= 1:
+    if user_number > 0:
         print("{} is a positive number.".format(user_number))
-    elif user_number <= -1:
+    elif user_number < 0:
         print("{} is a negative number.".format(user_number))
     elif user_number == 0:
         print("{} is just zero!".format(user_number))
